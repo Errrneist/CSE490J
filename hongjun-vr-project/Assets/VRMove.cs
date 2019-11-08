1237f7ad4a0618abb05b226f51e4b7604794c73f
Hongjun Wu <hongjunwu@hotmail.com>
2019-11-07 19:15:03 -0800

    Fix

---
+++ hongjun-vr-project/Assets/VRMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRMove : MonoBehaviour
+{
+    public float speed = 5;
+    public Rigidbody rigid;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+        rigid.MovePosition(transform.position + move * speed * Time.deltaTime);
+    }
+}

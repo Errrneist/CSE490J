052ed8cafe43db1f8f5a1f9e50fba126857e1af9
Hongjun Wu <hongjunwu@hotmail.com>
2019-11-20 20:38:46 -0800

    midterm_submission

--- hongjun-vr-project/Assets/Assignment 6/SelectableTyrantPlayer.cs
+++ hongjun-vr-project/Assets/Assignment 6/SelectableTyrantPlayer.cs
@@ -14,14 +14,14 @@ public class SelectableTyrantPlayer : Selectable
     // This method should set the pose parameter in the animator to poseNum 
     public override GameObject OnSelect()
     {
-        isSelected = true;
         // TODO:
         // Set the animator pose value here:
         // Set the object's mesh renderer to the selected material
         // Play the Tyrant's sound clip
-        
-
-
+        // Debug.Log("Selected");
+        isSelected = true;
+        gameObject.GetComponent<MeshRenderer>().material = selected;
+        sound.Play();
         return this.gameObject;
     }
 
@@ -29,14 +29,17 @@ public class SelectableTyrantPlayer : Selectable
     {
         // TODO:
         // Copy the code from Selectable Tile OnHover Here
-        
-
-
+        if (gameObject.GetComponent<MeshRenderer>().material != hover && !isSelected)
+        {
+            gameObject.GetComponent<MeshRenderer>().material = hover;
+        }
         return this.gameObject;
     }
 
     public override void OnDeselect()
     {
+        poseNum = 0;
+        gameObject.GetComponent<MeshRenderer>().material = def;
         isSelected = false;
         // TODO:
         // Set the animator's pose value to 0
